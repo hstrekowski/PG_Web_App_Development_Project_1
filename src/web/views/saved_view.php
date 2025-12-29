0dd58eb38ac534aff43d749979a93e5ef3742941
hstrekowski <hstrekowski05@gmail.com>
2025-12-29 23:55:34 +0100

    style: Adjusted saved_view.php styling
    Added more margin and styling for buttons, also added line between title and buttons

--- src/web/views/saved_view.php
+++ src/web/views/saved_view.php
@@ -5,8 +5,8 @@
         <form action="index.php?action=remove_selected" method="POST">
             
             <div class="cart-controls">
-                <button type="submit" class="btn-remove">Usuń zaznaczone z zapamiętanych</button>
-                <button type="submit" class="btn-update">Zapisz zmiany ilości</button>
+                <button type="submit" class="btn-remove">Usuń zaznaczone</button>
+                <button type="submit" class="btn-update">Zapisz ilości</button>
             </div>
 
             <div class="gallery-container">
@@ -16,7 +16,9 @@
                         <img src="images/<?php echo $img['thumbnail_name']; ?>" alt="Foto">
                         
                         <div class="photo-info">
-                            <strong><?php echo htmlspecialchars($img['title'] ?? 'Bez tytułu'); ?></strong>
+                            <strong style="font-size: 16px; display: block; margin-bottom: 5px;">
+                                <?php echo htmlspecialchars($img['title'] ?? 'Bez tytułu'); ?>
+                            </strong>
                             
                             <label class="remove-label">
                                 <input type="checkbox" name="remove_ids[]" value="<?php echo $id; ?>">
@@ -24,7 +26,7 @@
                             </label>
                             
                             <label class="qty-label">
-                                Ilość: 
+                                <span>Ilość sztuk:</span>
                                 <input type="number" name="quantities[<?php echo $id; ?>]" value="<?php echo $img['quantity']; ?>" min="1">
                             </label>
                         </div>
@@ -34,7 +36,7 @@
         </form>
     <?php else: ?>
         <div style="text-align: center; padding: 40px;">
-            <p>Nie masz żadnych zapamiętanych zdjęć.</p>
+            <p style="font-size: 18px; color: #666; margin-bottom: 20px;">Twój koszyk jest pusty.</p>
             <a href="index.php" class="btn-back">Wróć do galerii</a>
         </div>
     <?php endif; ?>
